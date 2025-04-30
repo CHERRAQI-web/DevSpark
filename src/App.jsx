3f74de642a65ac6b6985a5939c09c1c83dc02233
meryem boubghy <meryemboubghy02@gmail.com>
2025-04-30 13:08:33 +0100

    pull from the main

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import SectionHero from "./components/SectionHero";
 import Body from "./components/body";
 import QuiSommesnous from "./components/QuiSommesnous";
 import Contact from "./components/contact";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importing Router components
+import AjoutArticle from "./AjoutArticle";
 
 function App() {
   return (
     <Router>
-      {" "}
-      {/* Wrap the app in BrowserRouter */}
-      <div className="app">
-        <Navbar />
-        <main>
-          <SectionHero />
-          <Body />
-        </main>
-
-        {/* Set up routing here */}
+      <Navbar />
+      <main>
         <Routes>
-          <Route path="/" element={<Body />} /> {/* This is your homepage */}
-          <Route path="/contact" element={<Contact />} />{" "}
-          {/* This will show the contact page */}
-        </Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <SectionHero />
+                <Body />
+                <QuiSommesnous />
+              </>
+            }
+          />
+          <Route path="/contact" element={<Contact />} />
 
-        {/* Footer is common, so it remains at the bottom */}
-        <Footer />
-      </div>
+          <Route path="/créeblog" element={<AjoutArticle />} />
+        </Routes>
+      </main>
+      <Footer />
     </Router>
   );
 }
