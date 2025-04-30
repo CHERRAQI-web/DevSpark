3fa389de595ffeda94930f5ea3a5e5424d786459
meryem boubghy <meryemboubghy02@gmail.com>
2025-04-30 11:09:22 +0100

    Votre message de commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Footer from "./components/Footer";
 import SectionHero from "./components/SectionHero";
 import Navbar from "./components/Navbar/Navbar";
 import Contact from "./components/contact";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";  // Importing Router components
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importing Router components
 
 function App() {
   return (
-    <Router>  {/* Wrap the app in BrowserRouter */}
+    <Router>
+      {" "}
+      {/* Wrap the app in BrowserRouter */}
       <div className="app">
         <Navbar />
         <main>
@@ -19,10 +21,11 @@ function App() {
 
         {/* Set up routing here */}
         <Routes>
-          <Route path="/" element={<Body />} />  {/* This is your homepage */}
-          <Route path="/contact" element={<Contact />} />  {/* This will show the contact page */}
+          <Route path="/" element={<Body />} /> {/* This is your homepage */}
+          <Route path="/contact" element={<Contact />} />{" "}
+          {/* This will show the contact page */}
         </Routes>
-        
+
         {/* Footer is common, so it remains at the bottom */}
         <Footer />
       </div>
